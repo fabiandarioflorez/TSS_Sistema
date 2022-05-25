b93e893c9144aa7b95a0f86e5a570ce29db3a8bb
Fabian Florez <johndoe@example.com>
2022-05-24 19:32:33 -0500

    first commit

---
+++ config/config.py
@@ -0,0 +1,20 @@
+# Esto es la configuracion mediante objetos
+
+class Config(object):
+    'Esta es la configuracion base'
+    SECRET_KEY = 'Key'
+    DEBUG = True
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost:3306/pyalmacen"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
+
+class ProductionConfig(Config):
+    'Esta es la configuracion para Produccion'
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    'Esta es la configuracion para Desarrollo'
+    DEBUG = True
+    TESTING = True
+    SECRET_KEY = 'Desarrollo key'
+    
---
+++ run.py
@@ -0,0 +1,10 @@
+from app import app
+
+if __name__ == "__main__":
+    
+    # Llamamos la configuracion
+    # Configuracion modo desarrollo
+
+    # Ejecutamos la aplicacion
+    app.run()
+    
